b8a8c82f23c1da5b1d7b7d26844292f39e2e3c7e
JerryI <krikus.ms@gmail.com>
2023-05-27 23:37:39 +0200

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# WLJS Markdown support package
+**An extension for [WLJS-Editor](https://github.com/JerryI/wljs-editor) to add markdown and latex types of cells...**
+
+## License
+Project is released under the GNU General Public License (GPL).
---
+++ src/kernel.js
@@ -0,0 +1,56 @@
+import {marked} from "marked"
+import markedKatex from "marked-katex-extension"
+
+
+const TexOptions = {
+  throwOnError: false
+};
+
+
+marked.use(markedKatex(TexOptions));
+
+
+class MarkdownCell {
+    origin = {}
+
+    dispose() {
+      
+    }
+    
+    constructor(parent, data) {
+      parent.element.innerHTML = marked.parse(data);
+      parent.element.classList.add('padding-fix');
+      return this;
+    }
+  }
+  
+
+  window.SupportedLanguages.push({
+    check: (r) => {return(r[0] === '.md')},
+    plugins: [window.markdown()],
+    name: window.markdownLanguage.name
+  });
+
+  window.SupportedCells['markdown'] = {
+    view: MarkdownCell
+  };
+
+  /*globalExtensions.push(keymap.of([{ key: "Ctrl-7", run: function() {
+    return ({ state, dispatch }) => {
+      if (state.readOnly) return false;
+      let changes = state.changeByRange((range) => {
+        let { from, to } = range;
+        //if (atEof) from = to = (to <= line.to ? line : state.doc.lineAt(to)).to
+  
+        return {
+          changes: { from, to, insert: ".md\n" },
+          range: EditorSelection.cursor(from + 3)
+        };
+      });
+  
+      dispatch(
+        state.update(changes, { scrollIntoView: true, userEvent: "input" })
+      );
+      return true;
+    };    
+  } }]));*/
